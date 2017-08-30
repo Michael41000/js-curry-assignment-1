ee6c884d361daeabbdfec42cbd545d2cbfb8e429
unknown <mrollberg@ymail.com>
2017-08-30 17:50:59 -0500

    Got calculateTotal working with only reduce

--- lib.js
+++ lib.js
@@ -23,10 +23,42 @@ const listedPrice =
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
-  listings =>
+  listings => 
     carts => {
-      // TODO
-    }
+      console.log('Listings')
+      console.log(listings)
+
+      console.log('Carts')
+      return carts.reduce(
+        (customerCartTotals, currentCustomer) => {
+          console.log('\tCustomer: ' + currentCustomer.customer)
+          const totalItemsPrice = currentCustomer.items.reduce(
+            (total, currentItem, index) => {
+              console.log('\t\tItem ' + index + ': ' + currentItem)
+              return total + listings.reduce( 
+                (previousListing, currentListing) => {
+                  if (listedPrice(currentListing)(currentItem) !== 0)
+                  {
+                    console.log('\t\t\tPrice: ' + listedPrice(currentListing)(currentItem))
+                    return listedPrice(currentListing)(currentItem)
+                  }
+                  return previousListing  
+                }, 0
+              )
+            }, 0
+          )
+          const customer = {
+            'customer' : currentCustomer.customer,
+            'total' : totalItemsPrice
+          }
+          console.log('\tTotal Cart Price: ' + totalItemsPrice)
+          customerCartTotals.push(customer)
+          return customerCartTotals
+        }, []
+      )
+
+    
+  }
 
 module.exports = {
   listing,
