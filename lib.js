4f19366273ee4cedac12d56f9b95275cc972f4c9
unknown <mrollberg@ymail.com>
2017-08-30 20:39:57 -0500

    Refactored calculateTotals

--- lib.js
+++ lib.js
@@ -25,29 +25,15 @@ const listedPrice =
 const calculateTotals =
   listings => 
     carts => {
-      console.log('Listings')
-      console.log(listings)
-
-      console.log('Carts')
       return carts.map(
-        (currentCustomer) => {
-          console.log('\tCustomer: ' + currentCustomer.customer)
-          const totalItemsPrice = currentCustomer.items.reduce(
-            (total, currentItem, index) => {
-              console.log('\t\tItem ' + index + ': ' + currentItem)
-              return total + listings.find((listing) => listing.name === currentItem).price
-            }, 0
-          )
-          const customer = {
+        (currentCustomer) => { 
+          return {
             'customer' : currentCustomer.customer,
-            'total' : totalItemsPrice
+            'total' : currentCustomer.items.reduce(
+              (total, currentItem) => total + listings.find((listing) => listing.name === currentItem).price, 0)
           }
-          console.log('\tTotal Cart Price: ' + totalItemsPrice)
-          return customer
         }
       )
-
-    
   }
 
 module.exports = {
